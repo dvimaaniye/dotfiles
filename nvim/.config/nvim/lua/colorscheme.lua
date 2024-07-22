7fd98074ad61a69784353a9ff7f4a105786a8f5c
Gautam Tiwari <gautamtiwari.dev@gmail.com>
2024-07-22 22:45:12 +0530

    keeping it updated

--- nvim/.config/nvim/lua/colorscheme.lua
+++
@@ -1,8 +0,0 @@
-local colorscheme = "kanagawa-wave"
-
-local is_ok = pcall(vim.cmd, "colorscheme " .. colorscheme)
-
-if not is_ok then
-    vim.notify("colorscheme " .. colorscheme .. " not found!")
-    return
-end
