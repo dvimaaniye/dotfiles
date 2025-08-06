5183b4fa8449459a4c7c8faa43b922f001d204ae
Gautam Tiwari <gautamtiwari.dev@gmail.com>
2025-08-06 14:24:56 +0530

    no swapfile in nvim, adds media playing info on F9, removes tmux sessions

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -41,6 +41,8 @@ opt.undodir = vim.fn.expand("~/.local/share/nvim/undo")
 
 opt.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
+opt.swapfile = false
+
 -- opt.guicursor = "n-v-c:block,i-ci-ve:block,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
 
 setKey("n", "<leader><leader>x", "<cmd>source %<CR>")
